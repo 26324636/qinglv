ffb5f18be65640722001a97663fd7394428ad215
26324636 <26324636@qq.com>
2019-04-24 20:59:34 +0800

    首页

---
+++ config.js
@@ -0,0 +1,19 @@
+/**
+ * 小程序配置文件
+ */
+
+//正式
+var host = "https://www.sxscott.com/vehicle"
+//本地
+//var host ="http://smartdove.iask.in:17946/api"
+// var host ="http://192.168.43.160:8080/vehicle"
+//var host="https://buguanjiatest.site/api"
+//var host = "http://192.168.0.168:8080/"
+// var host = "http://192.168.43.160:8080/crazyBird"
+// var host = "http://192.168.1.102:8080/crazyBird"
+var config = {
+  host, 
+
+};
+
+module.exports = config
--- pages/start/index.js
+++ pages/start/index.js
@@ -1,50 +1,20 @@
-var url = require('../../config.js')
-const sendAjax = require('../../utils/sendAjax.js')
-var login = require('../../utils/wxlogin.js')
+// pages/start/index.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    userInfo: null,
-    isbound:1
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // login.wxLogin(0, function (res) {
-    //   that.setData({
-    //     userInfo: res,
-    //   })
-    // });
-    // var userInfo = wx.getStorageSync('userinfo');
-    // this.setData({
-    //   userInfo: userInfo
-    // })
+
   },
-  toYuanWang:function(){
-    wx.navigateTo({
-      url: '',
-    })
-  },
-  toTianQi: function () {
-    wx.navigateTo({
-      url: '../weather/weather',
-    })
-  },
-  toDianYing: function () {
-    wx.navigateTo({
-      url: '../movie/movie',
-    })
-  },
-  toYouXi: function () {
-    wx.navigateTo({
-      url: '../game/game',
-    })
-  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
